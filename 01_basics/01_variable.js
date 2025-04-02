7fb437b5ee25d11322cbe567a3e7a5a60a0aaa88
Agent1604 <vineet.kumar1604@gmail.com>
2025-04-02 19:09:07 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const AccountId = 846795
+let AccountEmail = "shubham@google.com"
+var AccountPassword = "14789632"
+AccountCity = "Delhi"
+let AccountState;
+
+// AccountId = 6
+
+AccountEmail = "jkm@jk.com"
+AccountPassword = "258963147"
+AccountCity = "nindo"
+
+
+console.log(AccountId);
+
+console.table({AccountId, AccountEmail, AccountPassword, AccountCity, AccountState})
